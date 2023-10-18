f874735584801410a33f86c7f1941455ed248c23
vanso <vovanso2004@gmail.com>
2023-10-18 16:22:38 +0700

    Them Giao Dien Home

--- app/src/main/java/com/project/spendmanagement/HomeFragment.java
+++ app/src/main/java/com/project/spendmanagement/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.project.spendmanagement;
 
+//import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.DatePicker;
+//import android.widget.EditText;
+//import java.util.Calendar;
 
 
 public class HomeFragment extends Fragment {
 
-
+  // private EditText dateEditText;
+ //  private Button datepickerButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+      //  setControl();
+        //setEvent();
         return inflater.inflate(R.layout.fragment_home2, container, false);
     }
-}
+
+//    private void setEvent() {
+//        // Su Kien Ngay Thang
+//
+//        datepickerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final Calendar calendar = Calendar.getInstance();
+//                int year = calendar.get(Calendar.YEAR);
+//                int month = calendar.get(Calendar.MONTH);
+//                int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year; // Format ngày tháng năm
+//                        dateEditText.setText(selectedDate);
+//                    }
+//                }, year, month, day);
+//
+//                datePickerDialog.show();
+//            }
+//        });
+    }
+
+//    private void setControl() {
+//        dateEditText = getView().findViewById(R.id.dateEditText);
+//        datepickerButton = getView().findViewById(R.id.datepickerButton);
+//    }
+
--- app/src/main/java/com/project/spendmanagement/MainActivity.java
+++ app/src/main/java/com/project/spendmanagement/MainActivity.java
@@ -1,14 +1,14 @@
 package com.project.spendmanagement;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,44 +16,47 @@ public class MainActivity extends AppCompatActivity {
     PageLich pageLich;
     PageBaoCao pageBaoCao;
     PageKhac pageKhac;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+           BottomNavigationView navigationView=findViewById(R.id.bottom_nav);
 
-        BottomNavigationView  navigationView=findViewById(R.id.bottom_nav);
-        // Khởi tạo các fragment
+        // Khoi Tao Cac Fragment
         homeFragment = new HomeFragment();
         pageLich = new PageLich();
         pageBaoCao = new PageBaoCao();
         pageKhac = new PageKhac();
-             getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
-           navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-               @Override
-               public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                   if (item.getItemId() == R.id.nhap) {
-                       getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                       return true;
-                   }
-                   if (item.getItemId() == R.id.lich) {
-                       getSupportFragmentManager().beginTransaction().replace(R.id.container, pageLich).commit();
-                       return true;
-                   }
-                   if (item.getItemId() == R.id.baocao) {
-                       getSupportFragmentManager().beginTransaction().replace(R.id.container, pageBaoCao).commit();
-                       return true;
-                   }
-                   if (item.getItemId() == R.id.khac) {
-                       getSupportFragmentManager().beginTransaction().replace(R.id.container, pageKhac).commit();
-                       return true;
-                   }
-                   return false;
-               }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        navigationView.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nhap) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
 
-           });
+                }
+                if (item.getItemId() == R.id.lich) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, pageLich).commit();
+                    return true;
+                }
+                if (item.getItemId() == R.id.baocao) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, pageBaoCao).commit();
+                    return true;
+                }
+                if (item.getItemId() == R.id.khac) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, pageKhac).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-             }
-        }
+    }
+
+}
 
 
 
