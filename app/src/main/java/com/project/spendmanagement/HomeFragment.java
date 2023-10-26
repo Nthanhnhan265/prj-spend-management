35749cff30f5f8fd5c9ea0ddd2dc4104422c502d
vanso <vovanso2004@gmail.com>
2023-10-26 13:38:20 +0700

    update giao dien home and lich

--- app/src/main/java/com/project/spendmanagement/HomeFragment.java
+++ app/src/main/java/com/project/spendmanagement/HomeFragment.java
@@ -1,60 +1,92 @@
 package com.project.spendmanagement;
 
-//import android.app.DatePickerDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-//import android.widget.Button;
-//import android.widget.DatePicker;
-//import android.widget.EditText;
-//import java.util.Calendar;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class HomeFragment extends Fragment {
 
-  // private EditText dateEditText;
- //  private Button datepickerButton;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
+  private Button datepickerButton;
 
-      //  setControl();
-        //setEvent();
-        return inflater.inflate(R.layout.fragment_home2, container, false);
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_home2, container, false);
+    setControl(view);
+    setEvent();
 
-//    private void setEvent() {
-//        // Su Kien Ngay Thang
-//
-//        datepickerButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                final Calendar calendar = Calendar.getInstance();
-//                int year = calendar.get(Calendar.YEAR);
-//                int month = calendar.get(Calendar.MONTH);
-//                int day = calendar.get(Calendar.DAY_OF_MONTH);
-//
-//                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-//                    @Override
-//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-//                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year; // Format ngày tháng năm
-//                        dateEditText.setText(selectedDate);
-//                    }
-//                }, year, month, day);
-//
-//                datePickerDialog.show();
-//            }
-//        });
-    }
+    return view;
+  }
 
-//    private void setControl() {
-//        dateEditText = getView().findViewById(R.id.dateEditText);
-//        datepickerButton = getView().findViewById(R.id.datepickerButton);
-//    }
+  private void setEvent() {
+    datepickerButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        showCustomDatePickerDialog();
+      }
+    });
 
+    datepickerButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+          @Override
+          public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+              Date date = sdf.parse(selectedDate);
+              SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+              String dayOfWeek = dayFormat.format(date); // Lấy thứ
+
+              // Xuất ngày và thứ
+              datepickerButton.setText(selectedDate + "("+ dayOfWeek+")");
+            } catch (ParseException e) {
+              e.printStackTrace();
+            }
+          }
+        }, year, month, day);
+
+        datePickerDialog.show();
+      }
+    });
+  }
+
+  private void setControl(View view) {
+
+    datepickerButton = view.findViewById(R.id.datepickerButton);
+
+  }
+  private void showCustomDatePickerDialog() {
+    final Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), R.style.CustomDatePickerDialogTheme, null, year, month, day);
+
+    // Đặt background cho DatePickerDialog từ drawable custom_background.xml
+    datePickerDialog.getDatePicker().setBackgroundResource(R.drawable.custom_background);
+
+    datePickerDialog.show();
+  }
+
+}
--- app/src/main/java/com/project/spendmanagement/PageLich.java
+++ app/src/main/java/com/project/spendmanagement/PageLich.java
@@ -1,5 +1,6 @@
 package com.project.spendmanagement;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,85 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class PageLich extends Fragment {
 
-
+    private Button datepickerButton;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_lich, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page_lich, container, false);
+        setControl(view);
+        setEvent();
+
+        return view;
     }
+
+    private void setEvent() {
+        datepickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomDatePickerDialog();
+            }
+        });
+
+        datepickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                        try {
+                            Date date = sdf.parse(selectedDate);
+                            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                            String dayOfWeek = dayFormat.format(date); // Lấy thứ
+
+                            // Xuất ngày và thứ
+                            datepickerButton.setText(selectedDate + "("+ dayOfWeek+")");
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    private void setControl(View view) {
+
+        datepickerButton = view.findViewById(R.id.datepickerButton);
+
+    }
+    private void showCustomDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), R.style.CustomDatePickerDialogTheme, null, year, month, day);
+
+        // Đặt background cho DatePickerDialog từ drawable custom_background.xml
+        datePickerDialog.getDatePicker().setBackgroundResource(R.drawable.custom_background);
+
+        datePickerDialog.show();
+    }
+
 }
